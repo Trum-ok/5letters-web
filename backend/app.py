037699d4d1fc7d20e1e9c38a9826c7b49d859f75
Trum <artamarkan@gmail.com>
2025-02-19 01:50:43 +0300

    app was updated

--- backend/app.py
+++ backend/app.py
@@ -1,29 +1,49 @@
 import random
+import argparse
 
-from flask import Flask, jsonify
 from flask_cors import CORS
+from flask import Flask, jsonify, Response
+# from elasticsearch import Elasticsearch
+
+from metrics import init_metrics
+from config import FLASK_PORT, PROMETHEUS_PORT
 
 app = Flask(__name__)
-CORS(app)
+cors = CORS(app)
+# es = Elasticsearch(["elasticsearch:9200"], timeout=30)
 
-with open("russian_words.txt", "r") as txt_file:
+# TODO: стоит ли всегда хранить его в считанном виде?
+with open("russian_words.txt", "r") as txt_file:  
     words = txt_file.read().split()
 
 
 @app.route("/")
-def main():
-    return "ok"
+def index() -> Response:
+    return jsonify({"status": "ok"}), 200
 
 
 @app.route("/get_random_word", methods=["GET"])
-def get_random_word():
+def get_random_word() -> Response:
     random_word = random.choice(words).upper()
     print(
         f'''
         word: {random_word}
         ''')
-    return jsonify({"word": random_word})
+    return jsonify({"word": random_word}), 200
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--port", default=FLASK_PORT, type=int)
+    parser.add_argument("--promport", default=PROMETHEUS_PORT, type=int)
+    parser.add_argument("--debug", default=False, type=bool)
+    args = parser.parse_args()
+    
+    init_metrics(app, port=args.promport)
+
+    app.debug = args.debug
+    app.run(host="0.0.0.0", port=args.port)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    main()
